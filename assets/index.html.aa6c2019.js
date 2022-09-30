ebd90a6b1040211cc943d19ae81b08eb2d91e0f4
Hkr32 <hkrtrk@yandex.ru>
2022-09-30 12:09:37 +0000

    Deploying to gh-pages from @ ruBootstrap/bootstrap-vue-3@34adaeb5af3bd26426b0e3cc54ca76665e659989 🚀

--- assets/index.html.aa6c2019.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-1020f180","path":"/components/","title":"\u041E\u0433\u043B\u0430\u0432\u043B\u0435\u043D\u0438\u0435","lang":"ru-RU","frontmatter":{},"excerpt":"","headers":[],"git":{"updatedTime":1664539091000},"filePathRelative":"components/README.md","componentReference":null}');export{e as data};
